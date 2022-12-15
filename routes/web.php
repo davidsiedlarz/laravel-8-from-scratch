64fc43f0dbe6015f178223825b9ef83d7b0f24ab
Dawid Siedlarz <dawidsiedlarz@gmail.com>
2022-12-15 23:13:21 +0100

    Episode 38 - Search(The Cleaner Way)

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,22 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);    
-});
+Route::get('/',[PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author']),
+        'posts' => $category->posts,
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);    
@@ -40,7 +30,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author']),
+        'posts' => $author->posts,
         'categories' => Category::all()
     ]);    
 });
